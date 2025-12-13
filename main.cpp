b6155c117c901bdb17dff18841d69917ab1fb323
Matvey <dfasaAZ@yandex.ru>
2025-12-13 04:01:04 +0300

    Исправлен алгоритм, заготовка под тесты

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "src/QuickSort.h"
 int main() {
-int array[10] = {1, 8, 3, 5, 4, 6, 7, 2, 9, 10};
-    sort(std::begin(array), std::end(array));
+    system("chcp 65001");
     return 0;
 }
--- src/QuickSort.h
+++ src/QuickSort.h
@@ -2,7 +2,7 @@
 #define SORTLAB_QUICKSORT_H
 #include <bits/shared_ptr_base.h>
 ///Кол-во элементов для смены сортировки
-const int THRESHOLD = 4;
+const int THRESHOLD = 20;
 
 /// Функция сравнивания
 /// @tparam T
@@ -21,10 +21,10 @@ bool compar(const T &a, const T &b) {
 /// @param last Конец коллекции
 /// @param comp чем сравниваем
 template<typename T, typename Compare>
-void insertionSort(T* first, T* last, Compare comp = compar) {
-    for (T* i = first + 1; i < last; ++i) {
+void insertionSort(T *first, T *last, Compare comp = compar) {
+    for (T *i = first + 1; i < last; ++i) {
         T value = std::move(*i);
-        T* j = i;
+        T *j = i;
         while (j > first && comp(value, *(j - 1))) {
             *j = std::move(*(j - 1));
             --j;
@@ -41,45 +41,47 @@ void insertionSort(T* first, T* last, Compare comp = compar) {
 /// @param comp чем сравниваем
 template<typename T, typename Compare>
 void sort(T *first, T *last, Compare comp) {
-    auto n = last - first;
-    if (n < 1) return;
-    if (n <= THRESHOLD) {
-        insertionSort(first, last, comp);
-        return;
-    }
-    //Медиана
-    T* pivot = first + (n / 2);
-    if (comp(*pivot, *first)) {
-        std::swap(*first, *pivot);
-    }
-    if (comp(*(last-1), *first)) {
-        std::swap(*first, *(last-1));
-    }
-    if (comp(*(last-1), *pivot)){
-        std::swap(*pivot, *(last-1));
-    }
-
-    // Меняем местами элементы
-    T* left = first;
-    T* right = last-1;
     while (true) {
-        while (comp(*left, *pivot))  ++left;
-        while (comp(*pivot, *right)) --right;
+        auto n = last - first;
+        if (n < 2) return;
+        if (n <= THRESHOLD) {
+            insertionSort(first, last, comp);
+            return;
+        }
 
-        if (left >= right) break;
-        std::swap(*left, *right);
-        ++left;
-        --right;
-    }
-    //Выбор какой кусок в какую сортировку
-    if (pivot - first < last - (pivot+1)) {
-        sort(first, pivot, comp);
-        insertionSort(pivot+1, last, comp);
-    } else {
-        sort(pivot+1, last, comp);
-        insertionSort(first,pivot, comp);
+        // Ищем медиану
+        T *mid = first + (n / 2);
+        if (comp(*mid, *first)) std::swap(*mid, *first);
+        if (comp(*(last - 1), *first)) std::swap(*(last - 1), *first);
+        if (comp(*(last - 1), *mid)) std::swap(*(last - 1), *mid);
+        T *pivot = last - 1;
+        T *left = first;
+        T *right = last - 2;
+
+        //меняем элементы
+        while (left <= right) {
+            while (comp(*left, *pivot)) ++left;
+            while (comp(*pivot, *right)) --right;
+
+            if (left >= right) break;
+
+            std::swap(*left, *right);
+            ++left;
+            --right;
+        }
+
+
+        std::swap(*left, *pivot);
+        T *pivot_pos = left;
+
+        //меньшее в рекурсию
+        if (pivot_pos - first < last - (pivot_pos + 1)) {
+            sort(first, pivot_pos, comp);
+            first = pivot_pos + 1;
+        } else {
+            sort(pivot_pos + 1, last, comp);
+            last = pivot_pos;
+        }
     }
 }
-
-
 #endif
